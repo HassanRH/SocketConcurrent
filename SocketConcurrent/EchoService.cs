4a24ef9c6bccf42719d594c3d0d7f3b3f2bc83f8
Hassan Raza Hussain <hass0568@edu.easj.dk>
2020-05-25 19:11:15 +0200

    SocketConcurrent

---
+++ SocketConcurrent/EchoService.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * EchoService.cs
+ *
+ * Author Michael Claudius, ZIBAT Computer Science
+ * Version 1.0. 2014.02.10
+ * Copyright 2014 by Michael Claudius
+ * Revised 2014.09.11
+ * All rights reserved
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+
+namespace SocketConcurrent
+{
+    class EchoService
+    {
+        private TcpClient connectionSocket;
+
+        public EchoService(TcpClient connectionSocket)
+        {
+            // TODO: Complete member initialization
+            this.connectionSocket = connectionSocket;
+        }
+        internal void DoIt()
+        {
+            Stream ns = connectionSocket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true; // enable automatic flushing
+
+            string message = sr.ReadLine();
+            string answer;
+            while (message != null && message != "")
+            {
+                Console.WriteLine("Client: " + message);
+                answer = message.ToUpper();
+                sw.WriteLine(answer);
+                message = sr.ReadLine();
+
+            }
+            ns.Close();
+            connectionSocket.Close();
+        }
+    }
+}
+
---
+++ SocketConcurrent/TCPEchoServer2.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * TcpEchoServer2.cs
+ *
+ * Author Michael Claudius, ZIBAT Computer Science
+ * Version 1.0. 2014.02.10
+ * Copyright 2014 by Michael Claudius
+ * Revised 2014.09.15, revised 2016.09.05, 2017.09.20
+ * All rights reserved
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SocketConcurrent
+{
+    public class TCPEchoServer2
+    {
+        public static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            TcpListener serverSocket = new TcpListener(ip, 6789);
+
+            //TcpListener serverSocket = new TcpListener(6789);
+            serverSocket.Start();
+
+
+            while (true)
+            {
+                TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+                Console.WriteLine("Server activated now");
+                EchoService service = new EchoService(connectionSocket);
+                //Use Task and delegates
+
+                //Task solution with delegates
+                Task.Factory.StartNew(() => service.DoIt());
+
+                //OR
+                //Task.Factory.StartNew(service.DoIt);
+
+                //OR
+                //Task.Run(( ) => service.DoIt());
+
+                //OR
+                //Thread solution
+                //Thread myThread = new Thread(new ThreadStart(service.DoIt));
+      
+                //OR
+                //Thread myThread = new Thread(service.DoIt);
+                //myThread.Start();
+
+            }
+
+            serverSocket.Stop();
+        }
+
+    }
+}
